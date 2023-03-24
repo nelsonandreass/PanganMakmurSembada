7d114cb0394f286a3d119a9da18742cac58635f1
SGO-NELSON\Lenovo <nelson@sgo.co.id>
2023-03-24 18:10:32 +0700

    update

--- resources/views/layouts/desc.blade.php
+++ resources/views/layouts/desc.blade.php
@@ -1,4 +1,5 @@
 <div class="section w-100 profile" id="profile">
+    <!-- on lg -->
     <div class="container-fluid p-0 m-0 d-none d-md-block">
         <div class="row " style="height:100vh !important;margin-left:10%;margin-right:10%;">
             <div class="col-md-6 my-auto ">
@@ -6,7 +7,7 @@
                     <img src="{{url('/assets/images/pexels-pixabay-164504.jpg')}}" class="img-desc">
                 </div>
             </div> 
-            <div class="col-md-6 my-auto px-5">
+            <div class="col-md-6 my-auto pl-5">
                 <h1 class="bold text-wrap"><b>PT. Pangan<br>Makmur Sembada</b></h1>
                 <div class="row">&nbsp;</div>
                 <p class="text-wrap">Merupakan perusahaan yang bergerak dibidang produksi & distribusi beras yang bertempat di Jakarta Dengan pengalaman lebih dari 30 tahun dalam dunia komoditi pangan. kami PT. Pangan Makmur Sembada selalu berusaha memberikan produk beras terbaik ke tangan para konsumen</p>
@@ -14,7 +15,8 @@
         </div>
     </div>
 
-    <div class="container p-0 m-0 d-block d-sm-none">
+    <!-- on sm -->
+    <!-- <div class="container p-0 m-0 d-block d-sm-none">
         <div class="row p-0 m-0" style="height:100vh !important;">
             <div class="col-md-6 my-3">
                 <img src="{{url('/assets/images/pexels-pixabay-164504.jpg')}}" class="img-desc-mobile">
@@ -26,7 +28,7 @@
                 <p class="text-wrap">Merupakan perusahaan yang bergerak dibidang <br> produksi & distribusi beras yang bertempat di Jakarta<br>Dengan pengalaman lebih dari 30 tahun dalam dunia <br> komoditi pangan. kami PT. Pangan Makmur Sembada<br>selalu berusaha memberikan produk beras terbaik ke <br>tangan para konsumen</p>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 
 <!-- <div class="row h-100 " id="profile" style="height:100vh !important;">
